802ec6281c364bf9d253a0a4890893f96047e563
anam <anambrur@gmail.com>
2024-04-30 01:05:23 +0600

    learn merge conflict

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import DashboardView from '@/views/DashboardView.vue'
 import AccountTypeListView from '@/views/account_type/AccountTypeListView.vue'
+import WithdrawalListView from '@/views/withdrawal/WithdrawalListView.vue'
+import CashDepositListView from '@/views/cash_deposit/CashDepositListView.vue'
 import AccountTypeAdd from '@/views/account_type/AccountTypeAdd.vue'
 
 const router = createRouter({
@@ -17,10 +19,20 @@ const router = createRouter({
       component: AccountTypeListView
     },
     {
+      path: '/dashboard/withdrawal',
+      name: 'withdrawal',
+      component: WithdrawalListView
+    },
+    {
+      path: '/dashboard/cashDeposit',
+      name: 'cashDeposit',
+      component: CashDepositListView
+    },
+    {
       path: '/dashboard/accountTypeAdd',
       name: 'accountTypeAdd',
       component: AccountTypeAdd
-    },
+    }
   ]
 })
 
