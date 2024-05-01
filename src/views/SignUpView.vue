f66caf182818340d2771324378b683333d17b731
anam <anambrur@gmail.com>
2024-05-02 00:49:11 +0600

    signup page add

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="container">
-        <div class="container card mt-5 ms-2">
+        <div class="container card">
             <div class="row justify-content-center">
                 <div class="col-md-12">
                     <div class="card-header mt-5">
@@ -84,7 +84,7 @@
                                     <div class="mb-3">
                                         <label for="nomineeName" class="form-label">Nominee Name</label>
                                         <input type="text" v-model="nomineeName" class="form-control" id="nomineeName"
-                                            placeholder="Enter Your Nominee Name">
+                                            placeholder="Enter Nominee Name">
                                         <p style="color:red" v-if="nomineeNameError">
                                             {{ nomineeNameError }}
                                         </p>
@@ -95,7 +95,7 @@
                                     <div class="mb-3">
                                         <label for="nomineePhone" class="form-label">Nominee Phone</label>
                                         <input type="text" v-model="nomineePhone" class="form-control" id="nomineePhone"
-                                            placeholder="Enter Your Nominee Mobile Number">
+                                            placeholder="Enter Nominee Mobile Number">
                                         <p style="color:red" v-if="nomineePhoneError">
                                             {{ nomineePhoneError }}
                                         </p>
@@ -108,7 +108,7 @@
                                     <div class="mb-3">
                                         <label for="nomineeNID" class="form-label">Nominee NID</label>
                                         <input type="text" v-model="nomineeNID" class="form-control" id="nomineeNID"
-                                            placeholder="Enter Your Nominee Name">
+                                            placeholder="Enter Nominee NID">
                                         <p style="color:red" v-if="nomineeNIDError">
                                             {{ nomineeNIDError }}
                                         </p>
@@ -119,7 +119,7 @@
                                     <div class="mb-3">
                                         <label for="document" class="form-label">Document</label>
                                         <input type="text" v-model="document" class="form-control" id="document"
-                                            placeholder="Enter Your Nominee Mobile Number">
+                                            placeholder="Enter Your Document">
                                         <p style="color:red" v-if="documentError">
                                             {{ documentError }}
                                         </p>
@@ -131,15 +131,15 @@
                                 <div class="col-md-6">
                                     <div class="mb-3">
                                         <label for="nomineeNid" class="form-label">Account Type</label>
-                                        <select class="form-select" v-model="accountType"
+                                        <select class="form-select" v-model="selectedAccountType"
                                             aria-label="Default select example">
-                                            <option selected>Open this select menu</option>
-                                            <option value="1">One</option>
-                                            <option value="2">Two</option>
-                                            <option value="3">Three</option>
+                                            <option disabled value="">Please select an account type</option>
+                                            <option v-for="(d, i) in accountType" :key="i" :value="d.id">
+                                                {{ d.account_type }}</option>
+
                                         </select>
-                                        <p style="color:red" v-if="nomineeNidError">
-                                            {{ nomineeNidError }}
+                                        <p style="color:red" v-if="accountTypeError">
+                                            {{ accountTypeError }}
                                         </p>
                                     </div>
 
@@ -147,14 +147,31 @@
                                 <div class="col-md-6">
                                     <div class="mb-3">
                                         <label for="formFileMultiple" class="form-label">Image</label>
-                                        <input class="form-control" type="file" id="formFileMultiple" multiple>
+                                        <input class="form-control" type="file" id="formFileMultiple"
+                                            @change="handleImageUpload">
+                                        <p style="color:red" v-if="imageError">
+                                            {{ imageError }}
+                                        </p>
                                     </div>
                                 </div>
                             </div>
 
                             <div class="row">
-                                <div class="col-md-12">
-                                    <button type="submit" @click="savaCustomer" class="btn btn-primary">Submit</button>
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label for="password" class="form-label">Password</label>
+                                        <input type="password" v-model="password" class="form-control" id="password"
+                                            placeholder="********">
+                                        <p style="color:red" v-if="passwordError">
+                                            {{ passwordError }}
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <button type="submit" @click="savaCustomer"
+                                            class="btn btn-primary form-control mt-4">Submit</button>
+                                    </div>
                                 </div>
                             </div>
                         </form>
@@ -168,11 +185,14 @@
 </template>
 
 <script>
+import bcrypt from 'bcryptjs';
 import axios from 'axios'
 export default {
     data() {
         return {
             url: 'http://127.0.0.1:8000/api/customer',
+            accountType: '',
+            selectedAccountType: '',
             name: '',
             email: '',
             phone: '',
@@ -182,74 +202,133 @@ export default {
             nomineeName: '',
             nomineePhone: '',
             nomineeNID: '',
-            accountType: '',
             document: '',
-            photo: ''
+            photo: null,
+            imageError: '',
+            nameError: '',
+            emailError: '',
+            phoneError: '',
+            addressError: '',
+            nidNumberError: '',
+            dateError: '',
+            nomineeNameError: '',
+            nomineePhoneError: '',
+            nomineeNIDError: '',
+            accountTypeError: '',
+            documentError: '',
+            password: '',
+            passwordError: '',
         }
-
-
     },
     mounted() {
-        // console.log('savaAccountType')
-        // this.savaAccountType();
+        this.getAccountType();
 
     },
     methods: {
-        savaCustomer() {
-            // const CustomerData = {
-            //     customer_name: this.name,
-            //     email: this.email,
-            //     mobile: this.phone,
-            //     address: this.address,
-            //     photo: this.photo,
-            //     nid_number: this.nidNumber,
-            //     date_of_birth: this.date,
-            //     nominee_name: this.nomineeName,
-            //     nominee_mobile: this.nomineePhone,
-            //     nominee_nid_number: this.nomineeNID,
-            //     document: this.document,
-            //     account_type_id: this.accountType,
-            // }
-            // console.log(CustomerData)
-
-            axios.post(`${this.url}`, {
-                customer_name: this.name,
-                email: this.email,
-                mobile: this.phone,
-                address: this.address,
-                photo: this.photo,
-                nid_number: this.nidNumber,
-                date_of_birth: this.date,
-                nominee_name: this.nomineeName,
-                nominee_mobile: this.nomineePhone,
-                nominee_nid_number: this.nomineeNID,
-                document: this.document,
-                account_type_id: this.accountType,
-            })
+        getAccountType() {
+            axios.get('http://127.0.0.1:8000/api/accountType')
                 .then(res => {
-                    // this.accountType = (res.data.data)
-                    console.log(res)
-                    // this.$router.push("/dashboard/accountType")
+                    this.accountType = (res.data.data)
                 })
 
         },
-        // handleSubmit() {
-        //     if (this.name.length < 1) {
-        //         this.nameError = 'Account type is required';
-        //         return;
-        //     }
-        // }
+        handleImageUpload(event) {
+            this.photo = event.target.files[0];
+        },
+        hash(password) {
+            return bcrypt.hashSync(password, 10);
+        },
+        savaCustomer() {
+            let formData = new FormData();
+            const hashedPassword = this.hash(this.password);
+            formData.append('customer_name', this.name);
+            formData.append('email', this.email);
+            formData.append('mobile', this.phone);
+            formData.append('address', this.address);
+            formData.append('photo', this.photo);
+            formData.append('nid_number', this.nidNumber);
+            formData.append('date_of_birth', this.date);
+            formData.append('nominee_name', this.nomineeName);
+            formData.append('nominee_mobile', this.nomineePhone);
+            formData.append('nominee_nid_number', this.nomineeNID);
+            formData.append('document', this.document);
+            formData.append('account_type_id', this.selectedAccountType);
+            formData.append('password', hashedPassword);
+
+            // console.log('FormData:', formData);
+            axios.post(`${this.url}`, formData)
+                .then(res => {
+                    // console.log(res)
+                    this.$router.push("/")
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+
+        },
+        handleSubmit() {
+            this.nameError = '';
+            this.emailError = '';
+            this.phoneError = '';
+            this.addressError = '';
+            this.nidNumberError = '';
+            this.dateError = '';
+            this.nomineeNameError = '';
+            this.nomineePhoneError = '';
+            this.documentError = '';
+            this.nomineeNIDError = '';
+            this.passwordError = '';
+            this.imageError = '';
+
+            if (this.name.length < 1) {
+                this.nameError = 'Name is required';
+            }
+            if (this.email.length < 1) {
+                this.emailError = 'Email is required';
+            }
+            if (this.phone.length < 1) {
+                this.phoneError = 'Phone is required';
+            }
+            if (this.address.length < 1) {
+                this.addressError = 'Address is required';
+            }
+            if (this.nidNumber.length < 1) {
+                this.nidNumberError = 'NID Number is required';
+            }
+            if (this.date.length < 1) {
+                this.dateError = 'Date is required';
+            }
+            if (this.nomineeName.length < 1) {
+                this.nomineeNameError = 'Nominee Name is required';
+            }
+            if (this.nomineePhone.length < 1) {
+                this.nomineePhoneError = 'Nominee Phone is required';
+            }
+            if (this.nomineeNID.length < 1) {
+                this.nomineeNIDError = 'Nominee NID is required';
+            }
+            if (this.document.length < 1) {
+                this.documentError = 'Document is required';
+            }
+            if (this.password.length < 1) {
+                this.passwordError = 'Password is required';
+            }
+            if (this.photo.length < 1) {
+                this.passwordError = 'Image is required';
+            }
+
+        }
 
     },
-    // watch: {
-    //     'name': function () {
-    //         if (this.name.length < 4) {
-    //             this.nameError = 'Account type Required'
-    //         } else {
-    //             this.nameError = ''
-    //         }
-    //     }
-    // }
+    watch: {
+        'name': function () {
+            if (this.name.length < 4) {
+                this.nameError = 'Name type Required'
+            } else {
+                this.nameError = ''
+            }
+        }
+    }
 
 }
 </script>
