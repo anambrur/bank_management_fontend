a8d219b86ac8a23d56a343a3c24733368d071e04
mdforhadhossain143 <143mdforhadhossain@gmail.com>
2024-05-06 12:53:56 -0700

    forhad frontend update

--- src/router/index.js
+++ src/router/index.js
@@ -39,6 +39,13 @@ import LogInView from '@/views/LogInView.vue'
 import SignUpView from '@/views/SignUpView.vue'
 import CustomerView from '@/views/Customer/CustomerView.vue'
 import LoanTypeEdit from '@/views/loan_type/LoanTypeEdit.vue'
+import CardTypeListView from '@/views/card_type/CardTypeListView.vue'
+import CardTypeAdd from '@/views/card_type/CardTypeAdd.vue'
+import CardTypeEdit from '@/views/card_type/CardTypeEdit.vue'
+import CardDetailsListView from '@/views/card/CardDetailsListView.vue'
+import CardDetailsEdit from '@/views/card/CardDetailsEdit.vue'
+import CardDetailsAdd from '@/views/card/CardDetailsAdd.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -236,7 +243,39 @@ const router = createRouter({
       path: '/dashboard/customerList/',
       name: 'customerList',
       component: CustomerView
-    }
+    },
+    {
+      path: '/dashboard/cardType/',
+      name: 'cardType',
+      component: CardTypeListView
+    },
+    {
+      path: '/dashboard/cardAdd',
+      name: 'cardAdd',
+      component: CardTypeAdd
+    },
+    {
+      path: '/dashboard/editCardType/:id',
+      name: 'editCardType',
+      component: CardTypeEdit,
+      props: true // Pass route params as props to the component
+    },
+    {
+      path: '/dashboard/cardDetails/',
+      name: 'cardDetails',
+      component: CardDetailsListView
+    },
+    {
+      path: '/dashboard/cardDetailsAdd',
+      name: 'cardDetailsAdd',
+      component: CardDetailsAdd
+    },
+    {
+      path: '/dashboard/editCardDetails/:id',
+      name: 'editCardDetails',
+      component: CardDetailsEdit,
+      props: true // Pass route params as props to the component
+    },
   ]
 })
 
