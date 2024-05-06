35af5dc16e75ac37541e112f6c141b28b9cc6c60
unknown <Sahim Badsha>
2024-05-06 12:52:27 +0600

    badsha work in deposit section

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,6 @@ import LoanAddView from '@/views/loan/LoanAddView.vue'
 import LoanProposalAddView from '@/views/loan_proposal/LoanProposalAddView.vue'
 import DepositTypeAdd from '@/views/deposit_type/DepositTypeAdd.vue'
 import DepositAdd from '@/views/deposit/DepositAdd.vue'
-
 import WithdrawalListView from '@/views/withdrawal/WithdrawalListView.vue'
 import CashDepositListView from '@/views/cash_deposit/CashDepositListView.vue'
 
